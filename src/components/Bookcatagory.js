fc04f6158f2d0c4501be50cebbf5423d420d07dc
Ram-1234 <ramnayan699@gmail.com>
2021-06-27 13:52:34 +0530

    modified

--- src/components/Bookcatagory.js
+++ src/components/Bookcatagory.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './style.css';
 
-export default function Bookcatagory() {
+export default function Bookcatagory(props) {
+    const [optvalue,setValue]=useState("")
+
+     //console.log(optvalue)
+     const selectOption=(e)=>{
+         setValue(e.target.value)
+         //console.log(e.target.value)
+     }
+
     return (
         <>
-        <div className="bookcategory">
-            <select className="category">
-                <option>Computer Scince</option>
-                <option>Story</option>
-                <option>Historical</option>
-                <option>Geographic</option>
-                <option>Technology</option>
+        <div   className="bookcategory">
+            <select   onChange={()=>props.onSubmit(props.optvalue)} className="category" onChange={selectOption} defaultValu={optvalue}>
+                <option value="computer">Computer</option>
+                <option value="story">Story</option>
+                <option value="travel">Travel</option>
+                <option value="heaven">Heaven</option>
+                <option value="horror">Horror</option>
+                <option value="faith">Faith</option>
+                <option value="fantasy">Fantasy</option>
+                <option value="nature">Nature</option>
+                <option value="culture">Culture</option>
+                <option value="romantic">Romantic</option>
             </select>
         </div>
         </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,28 +7,37 @@ import Data from './data.json';
 import Footer from './footer.js';
 //import Mode from './Mode';
 
-export default function Home() {
-
+export default function Home(props) {
+    console.log(props)
     let [colors,setColor]=useState("white");
     let [text,textColor]=useState("black");
     let [input,setInput]=useState("")
-    // let [Data,setData]=useState(Data)
-    
+    let [newData,setnewData]=useState(Data)
+     
+    let setinput=(e)=>{
+        setInput(e.target.value)
+    }
     function submitInput(){
-        setInput(input)
+        newData=Data.filter(item=>item.title.toLowerCase().slice(0,input.length)===input.toLowerCase())
+        //console.log(newData)
+        setnewData(newData);
     }
 
     function colorChange(){
-        if(colors=='white' || colors=='red'){
+        if(colors==='white' || colors==='red'){
             setColor('black');
             textColor('white');
-        }else if(colors=='black'){
+        }else if(colors==='black'){
             setColor('white');
             textColor('black');
-        }else if(colors=='black'){
+        }else if(colors==='black'){
             setColor('red');
             textColor('green');
         }
+    }
+
+    const selectBook=()=>{
+     console.log('hell')
     }
 
     return (
@@ -36,19 +45,19 @@ export default function Home() {
         <div className="main">
             <div className="headerbar">
             <h1 className="logo">Digital Library...</h1>
-            <Bookcatagory/>
+            <Bookcatagory onSubmit={selectBook}/>
            {/* serach bar */}
             <div className="searchbar" >
-            <input className='search' type="text" name={input} placeholder="Search..." />
+            <input className='search' type="text" onChange={setinput} value={input} placeholder="Search..." />
             <button className="inputbtn" onClick={submitInput} >search</button>
             </div>
             {/* <Mode/> */}
-            <input type="button" onClick={colorChange} style={{color:text}} className="colorsmode" value='color mode'/>
+            <input type="button" onClick={colorChange}  className="colorsmode" value='color mode'/>
             </div>
             {/* <h2>Book list</h2> */}
             <div style={{background:colors ,color:text}}  className="bookhome" >
                 {
-                   input.length ? "":Data.map((val)=>{
+                  input.length ? newData.map((val)=>{
                     return( 
                         <Book
                         color={text}
@@ -60,7 +69,20 @@ export default function Home() {
                         link={val.website}
                         />
                     )
-                })       
+                })  :
+                Data.map((val)=>{
+                    return( 
+                        <Book
+                        color={text}
+                        rates={val.rate}
+                        imgsrc={val.image}
+                        bstatus={val.status}
+                        title={val.title}
+                        status={val.writer}
+                        link={val.website}
+                        />
+                    )
+                })        
                 }
             </div>
             <Footer/> 
@@ -69,3 +91,6 @@ export default function Home() {
         </>
     )
 }
+
+
+// onChange={(e)=>setInput(e.target.value)}
