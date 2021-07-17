e0b3aad94bf5c8b7b876459223974152384b2393
ramnayan yadav <59009687+Ram-1234@users.noreply.github.com>
2021-07-17 10:08:22 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
     let [input,setInput]=useState("")
     let [newData,setnewData]=useState(Data)
     let [type ,setType]=useState("all")
-
+ 
    function defaultType(e){
         setType(e.target.value)
     } 
@@ -34,8 +34,9 @@ export default function Home() {
     }
 
     function submitInput(){
-        newData=Data.filter(item=>item.title.toLowerCase().slice(0,input.length)===input.toLowerCase())
-        setnewData(newData);
+        // newData=Data.filter(item=>item.title.toLowerCase().slice(0,input.length)===input.toLowerCase())
+        newData=Data.filter(item=>item.title.toLowerCase().includes(input.toLowerCase()))
+            setnewData(newData);
     }
 
     function colorChange(){
@@ -70,19 +71,20 @@ export default function Home() {
             {/* <h2>Book list</h2> */}
             <div style={{background:colors ,color:text}}  className="bookhome" >
                 {
-                newData.map((val)=>{
-                    return( 
-                        <Book
-                        color={text}
-                        rates={val.rate}
-                        imgsrc={val.image}
-                        bstatus={val.status}
-                        title={val.title}
-                        status={val.writer}
-                        link={val.website}
-                        />
-                    )
-                })
+                 newData.length>0 ?   
+                  newData.map((val)=>{
+                        return( 
+                            <Book
+                            color={text}
+                            rates={val.rate}
+                            imgsrc={val.image}
+                            bstatus={val.status}
+                            title={val.title}
+                            status={val.writer}
+                            link={val.website}
+                            />
+                        )
+                }) : <h1 style={{color: "#f48c06", fontFamily:"cursive" }}>Book Not Found!📓</h1>
 
                 }
             </div>
