8298a4585a0691ae8e4e927c29cea9e97c45af9a
Ram-1234 <ramnayan699@gmail.com>
2023-09-20 22:38:17 +0530

    fixing small changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense, lazy } from 'react';
 import Bookcatagory from './category/Bookcatagory';
 import './styles/style.css';
 import "./style.css"
-import Book from './book/Books';
+// import Book from './book/Books';
 import Data from './data/data.json';
 import Footer from './footer/footer.js';
 import Modal from "./modal/modal"
 import LightModeIcon from '@mui/icons-material/LightMode';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
+
+const Book = lazy(() => import('./book/Books'))
 
 export default function Home() {
     let [colors, setColor] = useState("white");
@@ -17,7 +19,7 @@ export default function Home() {
     let [newData, setnewData] = useState(Data)
     let [type, setType] = useState("all")
     let [showDetails, setShowDetails] = useState(false);
-    const [details, setDetails] = useState('');
+    const [details, setDetails] = useState({});
 
     function defaultType(e) {
         setType(e.target.value)
@@ -68,7 +70,7 @@ export default function Home() {
 
 
     return (
-        <>
+        <React.Fragment>
             <div className="main">
                 <div className="headerbar">
                     <h1 className="logo"><MenuBookIcon fontSize='32px' style={{ margin: "0 8px 0 0" }} /> Library</h1>
@@ -89,34 +91,36 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div style={{ background: colors, color: text }} className="bookhome" >
-                    {
-                        newData.length > 0 ?
-                            newData.map((val, ind) => {
-                                return (
-                                    <Book
-                                        key={ind}
-                                        id={ind}
-                                        color={text}
-                                        rates={val.rate}
-                                        imgsrc={val.image}
-                                        bstatus={val.status}
-                                        title={val.title}
-                                        status={val.writer}
-                                        link={val.website}
-                                        discreption={val.description}
-                                        showBookHandler={showBookHandler}
-                                    />
-                                )
-                            }) : <h1 style={{ color: "#f48c06", fontFamily: "cursive" }}>Book Not Found!📓</h1>
+                <Suspense fallback={<p>loading...</p>}>
+                    <div style={{ background: colors, color: text }} className="bookhome" >
+                        {
+                            newData.length > 0 ?
+                                newData.map((val, ind) => {
+                                    return (
+                                        <Book
+                                            key={ind}
+                                            id={ind}
+                                            color={text}
+                                            rates={val.rate}
+                                            imgsrc={val.image}
+                                            bstatus={val.status}
+                                            title={val.title}
+                                            status={val.writer}
+                                            link={val.website}
+                                            discreption={val.description}
+                                            showBookHandler={showBookHandler}
+                                        />
+                                    )
+                                }) : <h1 style={{ color: "#f48c06", fontFamily: "cursive" }}>Book Not Found!📓</h1>
 
-                    }
-                </div>
+                        }
+                    </div>
+                </Suspense>
                 {showDetails && <Modal closeHandle={closeDetails} data={details} />}
                 <Footer />
             </div>
             {/* <footer/>  */}
-        </>
+        </React.Fragment>
     )
 }
 
