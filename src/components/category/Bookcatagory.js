8601eb0546ad2479d426d30e3293d78fdd591303
Ram-1234 <ramnayan699@gmail.com>
2023-09-20 16:24:23 +0530

    clear folder structure

---
+++ src/components/category/Bookcatagory.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import '../styles/style.css';
+
+export default function Bookcatagory(props) {
+    const [optvalue] = useState("");
+
+
+    return (
+        <>
+            <div className="bookcategory">
+                <select onChange={(e) => props.defaultType(e)} className="category" defaultValue={optvalue}>
+                    <option value="all">All</option>
+                    <option value="story">Story</option>
+                    <option value="travel">Travel</option>
+                    <option value="heaven">Heaven</option>
+                    <option value="horror">Horror</option>
+                    <option value="faith">Faith</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="nature">Nature</option>
+                    <option value="culture">Culture</option>
+                    <option value="romantic">Romantic</option>
+                </select>
+            </div>
+        </>
+    )
+}
